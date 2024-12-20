c170431d2b54c3aabee2159bb98b8ca970f186e8
N1k1ko <beelinenikita@mail.ru>
2024-12-20 23:22:10 +0500

    init
    * create .gitignore
    * add SQLite-net-pcl

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Pekanum
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
